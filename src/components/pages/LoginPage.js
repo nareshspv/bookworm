2ff4595f68461b470c57a870371f7154ec2b4ac9
nareshspv <nareshspv@gmail.com>
2018-08-08 22:43:22 +0530

    make aync request

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,14 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import LoginForm from "../forms/LoginForm";
+import { login } from "../../actions/auth";
 
 class LoginPage extends React.Component {
-  submit = data => {
-    console.log(data);
-  };
+  submit = data =>
+    this.props.login(data).then(() => this.props.history.push("/"));
+
   render() {
     return (
       <div>
-        <h1>Login Page</h1>
+        <h1>Login page</h1>
 
         <LoginForm submit={this.submit} />
       </div>
@@ -16,4 +19,14 @@ class LoginPage extends React.Component {
   }
 }
 
-export default LoginPage;
+LoginPage.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  }).isRequired,
+  login: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { login }
+)(LoginPage);
